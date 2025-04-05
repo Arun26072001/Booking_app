2e5a3ba727f345427f8395424959c525db78c130
Arunkumar AD <arun@webnexs.in>
2025-04-05 14:28:30 +0530

    update live changes

--- bookingApp_backend/server.js
+++ bookingApp_backend/server.js
@@ -1,20 +1,31 @@
 const dotenv = require('dotenv');
-const app  = require("./app");
+const app = require("./app");
 const { DBConncetion } = require('./config/DatabaseConnection');
-dotenv.config({path: "./config/config.env"});
-const port  = process.env.PORT;
+dotenv.config({ path: "./config/config.env" });
+const port = process.env.PORT;
+const fs = require('fs');
+const https = require('https');
 
 // db conncetion
 DBConncetion();
 
-const server = app.listen(port, ()=>{
-    console.log(`${process.env.NODE_ENV} is running on ${port}`);
-})
+// const server = app.listen(port, () => {
+//     console.log(`${process.env.NODE_ENV} is running on ${port}`);
+// })
 
-process.on("unhandledRejection", (err)=>{
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/rbcstories.in/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/rbcstories.in/fullchain.pem')
+};
+
+https.createServer(options, app).listen(port, () => {
+    console.log(`✅ Server running on HTTPS at https://rbcstories.in:${port}`);
+  });
+
+process.on("unhandledRejection", (err) => {
     console.log(`something went wrong: ${err.message}`);
     console.log("app going to shut down!");
-    server.close(()=>{
+    server.close(() => {
         process.exit(1);
     })
 })
